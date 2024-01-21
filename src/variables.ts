394805c325ca8ab338294c9cf9f16755ac2ae417
jetie000 <makeenko.005@gmail.com>
2024-01-21 04:58:47 +0300

    feat: almost finish task

--- src/variables.ts
+++ src/variables.ts
@@ -5,5 +5,5 @@ export const variables = {
     DateToString: (date: Date) => date.getFullYear() +
         '-' + (date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) +
         '-' + (date.getDate() < 9 ? '0' + (date.getDate()) : (date.getDate())),
-    
+    PICS_PER_PAGE: 20
 }
